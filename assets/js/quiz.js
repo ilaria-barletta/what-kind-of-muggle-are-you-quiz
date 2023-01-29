fae400d94f80813ff41f95bd12f4b1add6c483c9
ilaria-barletta <ilariateresabarletta@gmail.com>
2023-01-29 13:59:08 +0000

    store user details when form is submitted

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,1 +1,16 @@
-console.log("Hello world")
+const detailsForm = document.getElementById("details-form");
+
+/**
+ * This funcion stores users details when they submit the wizard-details form 
+ */
+function detailsFormSubmit (event){
+    event.preventDefault();
+
+    const wizardName = detailsForm.elements['wname'].value;
+    const wizardEmail = detailsForm.elements['wemail'].value;
+    localStorage.setItem('name', wizardName);
+    localStorage.setItem('email', wizardEmail);
+    
+}
+
+detailsForm.addEventListener( 'submit', detailsFormSubmit); 
