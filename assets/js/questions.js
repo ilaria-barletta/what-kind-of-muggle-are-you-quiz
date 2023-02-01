3ea0210a70e27fddcbfa6516d39797dee108c7eb
ilaria-barletta <ilariateresabarletta@gmail.com>
2023-02-01 21:12:57 +0000

    add placeholder quiz form and quiz question

---
+++ assets/js/questions.js
@@ -0,0 +1,16 @@
+const quizQuestions = [
+    {
+        title: "Where did all begin?",
+        options: [
+            {
+                title: "12 Grimmauld Place"
+            }, 
+            {
+                title: "Hogsmeade's high street"    
+            },
+            {
+                title: "4 Private Drive"
+            }
+        ]
+    },
+]
