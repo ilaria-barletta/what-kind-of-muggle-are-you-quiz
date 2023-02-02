7ce4087debcba8ad8f76ceeffcf1bf5eb3fd4477
ilaria-barletta <ilariateresabarletta@gmail.com>
2023-02-02 18:01:58 +0000

    update quiz questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -6,18 +6,10 @@ const quizQuestions = [
                 title: "12 Grimmauld Place"
             }, 
             {
-                title: "Hogsmeade's high street"    
-            },
+                title: "All, what? I didn't understand the question"
+            }, 
             {
-                title: "4 Private Drive"
-            }
-        ]
-    },
-    {
-        title: "Where did all begin? 2",
-        options: [
-            {
-                title: "12 Grimmauld Place"
+                title: "Godric's Hollow, west country, England"
             }, 
             {
                 title: "Hogsmeade's high street"    
@@ -28,17 +20,383 @@ const quizQuestions = [
         ]
     },
     {
-        title: "Where did all begin? 3",
+        title: "How would you prefer to go places?",
         options: [
             {
-                title: "12 Grimmauld Place"
+                title: "Using a Broomstick"
             }, 
             {
-                title: "Hogsmeade's high street"    
+                title: "Using a portkey"
+            }, 
+            {
+                title: "pixie dust"
+            }, 
+            {
+                title: "oopsie daisy"    
             },
             {
-                title: "4 Private Drive"
+                title: "knight bus"
             }
         ]
     },
-]
+    {
+        title: "What's the best broom ever made",
+        options: [
+            {
+                title: "dyson cyclon v 10"
+            }, 
+            {
+                title: "Firebolt"
+            }, 
+            {
+                title: "Nimbus 2000"
+            }, 
+            {
+                title: "Nimbus 1000"    
+            },
+            {
+                title: "Roomba i7"
+            }
+        ]
+    },
+    {
+        title: "How would you make someone do what you want?",
+        options: [
+            {
+                title: "Expelliarmus"
+            }, 
+            {
+                title: "Please"
+            }, 
+            {
+                title: "Please, I beg you"
+            }, 
+            {
+                title: "Imperio"    
+            },
+            {
+                title: "Stupefy"
+            }
+        ]
+    },
+    {
+        title: "Who has proven to be the worst pet animal ever?",
+        options: [
+            {
+                title: "Nagini the snake"
+            }, 
+            {
+                title: "Trevor the toad"
+            }, 
+            {
+                title: "Crookshanks the cat"
+            }, 
+            {
+                title: "Scrabbers the rat"    
+            },
+            {
+                title: "Scooby the dog"
+            }
+        ]
+    },
+    {
+        title: "Which one, in the list, is not a deathly hallow?",
+        options: [
+            {
+                title: "The invisible cloack"
+            }, 
+            {
+                title: "Petrifying pain"
+            }, 
+            {
+                title: "The elden wand"
+            }, 
+            {
+                title: "The philosopher stone"    
+            },
+            {
+                title: "the resurrection stone"
+            }
+        ]
+    },
+    {
+        title: "Who is the most hated teacher amongst wizards and muggles?",
+        options: [
+            {
+                title: "Severus Snape"
+            }, 
+            {
+                title: "Dolores Umbridge"
+            }, 
+            {
+                title: "Lockhart"
+            }, 
+            {
+                title: "Pomona Sprout"    
+            },
+            {
+                title: "Dudley"
+            }
+        ]
+    },
+    {
+        title: "Which one, here, is not a Weasley?",
+        options: [
+            {
+                title: "Ginny"
+            }, 
+            {
+                title: "Rolf"
+            }, 
+            {
+                title: "Percy"
+            }, 
+            {
+                title: "Bill"    
+            },
+            {
+                title: "John"
+            }
+        ]
+    },
+    {
+        title: "What is Harry's Patronus ?",
+        options: [
+            {
+                title: "A stag"
+            }, 
+            {
+                title: "A doe"
+            }, 
+            {
+                title: "A dementor"
+            }, 
+            {
+                title: "A silver fox"    
+            },
+            {
+                title: "A full moon"
+            }
+        ]
+    },
+    {
+        title: "If you were born of the same blood status as You-Know-Who, what would you be?",
+        options: [
+            {
+                title: "AB+"
+            }, 
+            {
+                title: "Pure blood"
+            }, 
+            {
+                title: "Half blood"
+            }, 
+            {
+                title: "Muggle born"    
+            },
+            {
+                title: "Muggle"
+            }
+        ]
+    },   
+    {
+        title: "Who did not take part in the Triwizard Tournament?",
+        options: [
+            {
+                title: "Harry"
+            }, 
+            {
+                title: "Fred"
+            }, 
+            {
+                title: "Victor"
+            }, 
+            {
+                title: "Fleur"    
+            },
+            {
+                title: "Cedric"
+            }
+        ]
+    },   
+    {
+        title: "What is Harry's dream job?",
+        options: [
+            {
+                title: "Hogwarts teacher"
+            }, 
+            {
+                title: "Minister of magic"
+            }, 
+            {
+                title: "Auror"
+            }, 
+            {
+                title: "Hogwarts headmaster"    
+            },
+            {
+                title: "Goblin"
+            }
+        ]
+    },   
+    {
+        title: "What's the main ingredient in Harry's wand?",
+        options: [
+            {
+                title: "A phoenix feather"
+            }, 
+            {
+                title: "Unicorn hair"
+            }, 
+            {
+                title: "Dragon heartstring"
+            }, 
+            {
+                title: "A drop of You-Know-Who blood"    
+            },
+            {
+                title: "Basilisk scale"
+            }
+        ]
+    },   
+    {
+        title: "Which one, here, is not a ball used in quidditch?",
+        options: [
+            {
+                title: "Snitch"
+            }, 
+            {
+                title: "Bludger"
+            }, 
+            {
+                title: "Yule"
+            }, 
+            {
+                title: "Oval"    
+            },
+            {
+                title: "Quaffle"
+            }
+        ]
+    },   
+    {
+        title: "What's the name of Hagrid's dog?",
+        options: [
+            {
+                title: "Fang"
+            }, 
+            {
+                title: "Buckbeak"
+            }, 
+            {
+                title: "Fluffy"
+            }, 
+            {
+                title: "Aragog"    
+            },
+            {
+                title: "Norbert"
+            }
+        ]
+    },   
+    {
+        title: "What did Dobby get from Harry that set him free from Lucius Malfoy?",
+        options: [
+            {
+                title: "The oak wand"
+            }, 
+            {
+                title: "A glove"
+            }, 
+            {
+                title: "A sock"
+            }, 
+            {
+                title: "A scarf"    
+            },
+            {
+                title: "A pebble"
+            }
+        ]
+    },   
+    {
+        title: "If you were born of the same blood status as You-Know-Who, what would you be?",
+        options: [
+            {
+                title: "AB+"
+            }, 
+            {
+                title: "Pure blood"
+            }, 
+            {
+                title: "Half blood"
+            }, 
+            {
+                title: "Muggle born"    
+            },
+            {
+                title: "Muggle"
+            }
+        ]
+    },   
+    {
+        title: "Who is Harry's godfather?",
+        options: [
+            {
+                title: "Sirius Black"
+            }, 
+            {
+                title: "Remus Lupin"
+            }, 
+            {
+                title: "Peter Pettigrew"
+            }, 
+            {
+                title: "Albus Dumbledore"    
+            },
+            {
+                title: "Severus Snape"
+            }
+        ]
+    },   
+    {
+        title: "What's the name of the powerful potion that can change your appearance into that of someone else",
+        options: [
+            {
+                title: "Felix felicis"
+            }, 
+            {
+                title: "Fungiface Potion"
+            }, 
+            {
+                title: "Malevolent mixture"
+            }, 
+            {
+                title: "Polijuice potion"    
+            },
+            {
+                title: "Skele-Gro"
+            }
+        ]
+    },   
+    {
+        title: "Finish the quote: 'After all this time?'",
+        options: [
+            {
+                title: "Yes"
+            }, 
+            {
+                title: "Always"
+            }, 
+            {
+                title: "Sometimes"
+            }, 
+            {
+                title: "I'm afraid so"    
+            },
+            {
+                title: "Whatever"
+            }
+        ]
+    },   
+];
